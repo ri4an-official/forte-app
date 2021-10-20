93f628f67beb31c158a917de206b7d59dd048a3a
Ri4an <vadimguber7@gmail.com>
2021-10-20 14:58:10 +0600

    feat: add my hook for template inputs

---
+++ src/models/hooks/useInput.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+
+export const useInput = (initValue = 'password123#My') => {
+    const [value, setValue] = useState(initValue)
+    const [error, setError] = useState('')
+    const [firstInp, setFirstInp] = useState(false)
+    useEffect(() => {
+        if (
+            value &&
+            (value.includes('#') ||
+                value.includes('№') ||
+                value.includes('$') ||
+                value.includes('%') ||
+                value.includes(':') ||
+                value.includes('?') ||
+                value.includes('*') ||
+                value.includes('(') ||
+                value.includes(')')) &&
+            !value.includes('qwerty') &&
+            !value.includes('123456') &&
+            /d/.test(value) &&
+            /[A-Z]/.test(value)
+        )
+            setError('')
+        else if (firstInp && value.split('').some((s) => s !== s.toUpperCase()))
+            setError('Пароль не соответствует требованиям безопасности')
+    }, [firstInp, value])
+    return {
+        value,
+        onChange: (e: any) => {
+            setValue(e.target.value)
+            setFirstInp(true)
+        },
+        error,
+    }
+}
