9c05677191648e11d995743e3917f08ba15f9cf8
Ri4an <vadimguber7@gmail.com>
2021-10-20 14:57:15 +0600

    feat: styled app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import styled from 'styled-components'
 import { PasswordForm } from './components/PasswordForm'
 
 export const App = () => (
-    <div>
+    <StyledApp>
         <PasswordForm />
-    </div>
+    </StyledApp>
 )
+const StyledApp = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
