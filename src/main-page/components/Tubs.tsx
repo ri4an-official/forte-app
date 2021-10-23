cd2a7c24c2e0ded1cd524e14c3491e807294bf57
Ri4an <vadimguber7@gmail.com>
2021-10-23 21:08:12 +0600

    feat: fill tubs

--- src/main-page/components/Tubs.tsx
+++ src/main-page/components/Tubs.tsx
@@ -1,3 +1,55 @@
-export const Tubs = () => {
-    return <div></div>
-}
+import styled from 'styled-components'
+import { Card } from '../assets/icons/tubs/Card'
+import { Contsacts } from '../assets/icons/tubs/Contacts'
+import { Deposite } from '../assets/icons/tubs/Deposite'
+import { ForteFamily } from '../assets/icons/tubs/ForteFamily'
+import { Insurance } from '../assets/icons/tubs/Insurance'
+import { Premiere } from '../assets/icons/tubs/Premiere'
+import { Transfer } from '../assets/icons/tubs/Transfer'
+
+export const Tubs = () => (
+    <StyledTubs>
+        <Tub>
+            <Card />
+            Карты
+        </Tub>
+        <Tub>
+            <Deposite />
+            Депозит
+        </Tub>
+        <Tub>
+            <Transfer />
+            Перевод
+        </Tub>
+        <Tub>
+            <Insurance />
+            Insurance
+        </Tub>
+        <Tub>
+            <Premiere />
+            Premiere
+        </Tub>
+        <Tub>
+            <ForteFamily />
+            Forte Family
+        </Tub>
+        <Tub>
+            <Contsacts />
+            Контакты
+        </Tub>
+    </StyledTubs>
+)
+const StyledTubs = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 88px;
+`
+const Tub = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 0.08;
+`
