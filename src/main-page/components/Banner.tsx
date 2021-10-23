2d051b3ac0e6a353ce661c37e8165754458f973a
Ri4an <vadimguber7@gmail.com>
2021-10-23 21:07:32 +0600

    feat: add template for banner

--- src/main-page/components/Banner.tsx
+++ src/main-page/components/Banner.tsx
@@ -1,3 +1,29 @@
-export const Banner = () => {
-    return <div></div>
-}
+import styled from 'styled-components'
+import { BgImage } from '../assets/icons/banner/BgImage'
+
+export const Banner = () => (
+    <StyledBanner>
+        <Name>
+            <span>Forte</span> - Bank
+        </Name>
+        <BgImage />
+    </StyledBanner>
+)
+const StyledBanner = styled.div`
+    background-color: blue;
+    color: white;
+    width: 100%;
+    height: 400px;
+`
+const Name = styled.div`
+    font-size: 12px;
+    span {
+        text-decoration: underline;
+    }
+`
+const Block = styled.div`
+    height: 100%;
+    * {
+        margin: 10px;
+    }
+`
