65e6090ca84750f07523209f93fae517c46968b8
Ri4an <vadimguber7@gmail.com>
2021-10-25 03:52:37 +0600

    feat: fill Cards components

--- src/main-page/components/Cards/CardItem.tsx
+++ src/main-page/components/Cards/CardItem.tsx
@@ -1,3 +1,25 @@
-export const CardItem = () => {
-    return <div></div>
-}
+import { FC } from 'react'
+import styled from 'styled-components'
+
+export const CardItem = (props: { Card: FC<any>; title: string; sum: string }) => (
+    <div>
+        <props.Card />
+        <h2>{props.title}</h2>
+        <div>Выпуск карты бесплатно</div>
+        <div>Снятие до {props.sum} ₸ в месяц Казахстана бесплатно</div>
+        <div>CashBack до 15%</div>
+        <StyledButton>Заказать карту</StyledButton>
+    </div>
+)
+const StyledButton = styled.button`
+    width: 186px;
+    height: 44px;
+    text-transform: none;
+    font-size: 16px;
+    font-size: 16px;
+    border-radius: 4px;
+    background-color: #1e2a41;
+    color: white;
+    border: none;
+    cursor: pointer;
+`
--- src/main-page/components/Cards/Cards.tsx
+++ src/main-page/components/Cards/Cards.tsx
@@ -1,3 +1,34 @@
-export const Cards = () => {
-    return <div></div>
-}
+import styled from 'styled-components'
+import { Classic } from '../../assets/icons/cards/Classic'
+import { Gold } from '../../assets/icons/cards/Gold'
+import { Platinum } from '../../assets/icons/cards/Platinum'
+import { CardItem } from './CardItem'
+
+// FIXME - fix card
+export const Cards = () => (
+    <Styled>
+        <h1>Forte карты</h1>
+        <StyledCards>
+            <CardItem Card={() => <Classic />} title='Classic' sum='300 000' />
+            <CardItem Card={() => <Gold />} title='Gold' sum='500 000' />
+            <CardItem Card={() => <Platinum />} title='Platinum' sum='3 000 000' />
+        </StyledCards>
+    </Styled>
+)
+const Styled = styled.div`
+    margin-top: 50px;
+    margin-left: 100px;
+    margin-right: 100px;
+    h1 {
+        color: #1e2a41;
+        font-size: 30px;
+    }
+`
+const StyledCards = styled.div`
+    display: flex;
+    justify-content: center;
+    * {
+        text-align: center;
+        margin: 20px;
+    }
+`
