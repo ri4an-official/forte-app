2e8d369f60beacef50d6385e689f1d2157d25c5c
Ri4an <vadimguber7@gmail.com>
2021-10-26 03:41:59 +0600

    fix: add classes

--- src/main-page/assets/icons/banner/SelectedItem.tsx
+++ src/main-page/assets/icons/banner/SelectedItem.tsx
@@ -1,18 +1,17 @@
-export const PaginationItem = () => (
+export const SelectedItem = () => (
     <svg
-        className='item'
         width='44'
-        height='3'
-        viewBox='0 0 44 3'
+        height='5'
+        viewBox='0 0 44 5'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
     >
         <rect
-            width='3'
+            width='5'
             height='44'
-            rx='1.5'
+            rx='2.5'
             transform='matrix(4.37114e-08 1 1 -4.37114e-08 0 0)'
-            fill='white'
+            fill='#1E2A41'
         />
     </svg>
 )
