dbc83bf273f4ae7e46bed33d1ba074c9ee33deb4
Ri4an <vadimguber7@gmail.com>
2021-10-26 03:44:21 +0600

    feat: add points

--- src/main-page/components/Cards/Cards.tsx
+++ src/main-page/components/Cards/Cards.tsx
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 import { Classic } from '../../assets/icons/cards/Classic'
 import { Gold } from '../../assets/icons/cards/Gold'
 import { Platinum } from '../../assets/icons/cards/Platinum'
+import { Point } from '../../assets/icons/cards/Point'
 import { CardItem } from './CardItem'
 
-// FIXME - fix card
 export const Cards = () => (
     <Styled>
         <h1>Forte карты</h1>
@@ -13,8 +13,27 @@ export const Cards = () => (
             <CardItem Card={() => <Gold />} title='Gold' sum='500 000' />
             <CardItem Card={() => <Platinum />} title='Platinum' sum='3 000 000' />
         </StyledCards>
+        <Points>
+            <Point></Point>
+            <Point></Point>
+            <Point></Point>
+        </Points>
     </Styled>
 )
+const Points = styled.div`
+    display: flex;
+    justify-content: center;
+    * {
+        cursor: pointer;
+        margin: 7px;
+        padding: 4px;
+        border: 1px solid white;
+        border-radius: 20px;
+        &:hover {
+            border: 1px solid black;
+        }
+    }
+`
 const Styled = styled.div`
     margin-top: 50px;
     margin-left: 100px;
@@ -27,6 +46,13 @@ const Styled = styled.div`
 const StyledCards = styled.div`
     display: flex;
     justify-content: center;
+    img {
+        border-radius: 13px;
+        cursor: pointer;
+        width: 288px;
+        height: 179px;
+        box-shadow: 0px 6px 15px 1px darkgray;
+    }
     * {
         text-align: center;
         margin: 20px;
